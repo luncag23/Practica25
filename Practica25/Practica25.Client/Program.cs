f35dc1c1a1d6a2a00faa7fc64d72982b7bd038d3
Gheorghe <gheorghe.lunca@isa.utm.md>
2025-09-09 16:58:32 +0300

    asd

--- Practica25/Practica25.Client/Program.cs
+++ Practica25/Practica25.Client/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+// Configure HttpClient for API calls
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
